99127aefd17ef5aa8655dab3c731e6c740d8bc5b
Andrew Dooley <122635716+andrewdooley@users.noreply.github.com>
2023-04-29 19:38:20 -0400

    clickswork1

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 async function updateClickCount() {
   const response = await chrome.runtime.sendMessage({ type: "click" });
   document.getElementById("numclicks").innerHTML = response.count;
+  if (response.count > 5) {
+    document.getElementById("update").innerText = "time to switch hands";
+  }
 }
 
 window.addEventListener("click", updateClickCount);
